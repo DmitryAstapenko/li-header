ea1ddeeb570b42266bdb54c66b0060b926932141
DmitryAstapenko <dmitryastapenko1@gmail.com>
2021-05-04 16:43:06 +0300

    feat: add images in App

--- src/ElementIcon/ElementIcon.tsx
+++ src/ElementIcon/ElementIcon.tsx
@@ -1,11 +1,18 @@
 import * as React from "react";
+import "./ElementIcon.scss";
 
 interface IElementIconProps {
   url: string;
 }
 
-const ElementIcon: React.FunctionComponent<IElementIconProps> = ({ url }) => {
-  return <img src={url} alt="icon" />;
+const ElementIcon: React.FunctionComponent<IElementIconProps> = ({
+  url,
+}: IElementIconProps) => {
+  return (
+    <div className="element-icon">
+      <img src={url} alt="icon" />
+    </div>
+  );
 };
 
 export default ElementIcon;
